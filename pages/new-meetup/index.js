f342231e95fc711ccc1b955b3e13dfa9ea6731b9
Rafael Dela Cruz <rafael_delacruz1992@yahoo.com>
2021-07-25 00:30:59 +0400

    Ready for deployment

---
+++ pages/new-meetup/index.js
@@ -0,0 +1,27 @@
+import {Fragment} from 'react';
+import {useRouter} from 'next/router';
+
+import NewMeetupForm from '../../components/meetups/NewMeetupForm';
+
+function newMeetupPage(){
+    const router = useRouter();
+
+    async function addMeetupHander(enteredMeetupData){
+        const response = await fetch('/api/new-meetup', {
+            method: 'POST',
+            body: JSON.stringify(enteredMeetupData),
+            headers:{ 
+                'Content-Type': 'application/json'
+            }
+        });
+
+        const data = await response.json();
+        console.log(data);
+
+        router.push('/');
+    }
+
+    return  <NewMeetupForm onAddMeetup={addMeetupHander}/>
+}
+
+export default newMeetupPage;
